e86eb9ccc7d95a9980e6eb8b0690ba555bfb566d
Rodrigo Soares <rodrigo.soares0642@outlook.com.br>
2022-02-17 00:54:35 -0300

    Installed the react modal, imported, declared, created functionalities to open and close the modal, applied the modal to the button with its respective properties.

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,15 +1,37 @@
+import { useState } from 'react'
 import logoimg from '../../assets/logo.svg'
 import { Container, Content } from './style'
+import Modal from 'react-modal';
 
 
 export function Header (){
+
+  const [IsNewTransactionModalOpen, SetIsNewTransactionModalOpen] = useState(false);
+
+  function handleOpenNewTransactionModal (){
+    SetIsNewTransactionModalOpen(true);
+    
+  }
+
+  function handleCloseCNewTransactionModal (){
+    SetIsNewTransactionModalOpen(false);
+
+  }
+
   return(
     <Container>
       <Content>
         <img src={logoimg}  alt="dt money"></img>
-        <button type="button">
+        <button type="button" onClick={handleOpenNewTransactionModal}>
           Nova Transação
         </button>
+
+        <Modal 
+        isOpen={IsNewTransactionModalOpen}
+        onRequestClose={handleCloseCNewTransactionModal}
+        >
+          <h2>Cadastrar Transações</h2>
+        </Modal>
       </Content>
     </Container>
  
