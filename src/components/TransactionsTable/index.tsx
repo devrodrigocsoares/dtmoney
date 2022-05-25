feb7b097e8b2c29587458929563b491b2773e628
Rodrigo Soares <rodrigo.soares0642@outlook.com.br>
2022-05-25 13:05:54 -0300

    Formatted currency and date values

--- src/components/TransactionsTable/index.tsx
+++ src/components/TransactionsTable/index.tsx
@@ -38,9 +38,13 @@ export function TransactionsTable(){
             return(
               <tr key={transaction.id}>
                 <td>{transaction.title}</td>
-                <td className={transaction.type}>{transaction.amount}</td>
+                <td className={transaction.type}>
+                  {new Intl.NumberFormat('pt-Br', {
+                    style: 'currency',
+                    currency: 'BRL'
+                  }).format(transaction.amount)}</td>
                 <td>{transaction.category}</td>
-                <td>{transaction.createdAt}</td>
+                <td>{new Intl.DateTimeFormat('pt-Br').format(new Date(transaction.createdAt))}</td>
               </tr>
             )
           })
